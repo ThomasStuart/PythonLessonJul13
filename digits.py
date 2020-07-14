a98f253e7b59c2b7e4350951e511a4c8525f2b93
Thoms Stuart <thomas.j.stuart96@gmail.com>
2020-07-13 19:30:46 -0700

    Create digits.py

---
+++ digits.py
@@ -0,0 +1,36 @@
+def getRidOfLastNum( n ):
+    return n // 10
+
+def getLastNumber( n ):
+    return n % 10
+
+# What operator and number do the job
+n = 2342
+print ( getRidOfLastNum( n ) )   #  234
+print ( getLastNumber( n ) )     #  2
+
+m = 41
+print ( getRidOfLastNum( m ) )   #  4
+print ( getLastNumber( m ) )     #  1
+
+k = 128
+print ( getRidOfLastNum( k ) )   #  12
+print ( getLastNumber( k ) )     #  8
+
+
+
+def digits( number ):
+    nums = []
+    
+    # Process that we repeat,  when do we stop ?? 
+        # take off the last number 
+        # get ride of the last number
+    
+    # before we return nums, we have to reverse the list 
+    return nums
+
+
+print( digits(2342) ) # [2, 3, 4, 2]
+print( digits(1234) ) # [1 , 2, 3, 4]
+print( digits(12))    # [1 , 2]
+print( digits(9) )    # [ 9 ]
